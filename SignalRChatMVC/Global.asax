22bec06abfa9d12a1e87ffa4bddfbd550ebae2c8
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2018-11-21 22:20:22 +0530

    Initial commit

---
+++ SignalRChatMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRChatMVC.Global" Language="C#" %>
---
+++ SignalRChatMVC/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatMVC
+{
+    public class ChatHub : Hub
+    {
+        //chat.server.send($('#displayname').val(), $('#message').val());
+        //means you below method shoud be same as script chat.server.send() method
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+
+            //chat.client.addNewMessageToPage = function(name, message) should mathc below method
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
